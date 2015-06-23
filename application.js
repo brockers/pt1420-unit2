d0ad8bb57ea8870bf2168184c0b0fe3f526005c2
Bobby Rockers <103instructor@103LAB06WKS07.Campus.ittesi.com>
2015-06-22 22:36:26 -0500

    Not quite a mad libs example

--- application.js
+++ application.js
@@ -2,8 +2,20 @@
 function processForm(){
 	var name;
 	var message;
+	var content;
+	
 	name = document.getElementById("name");
 	message = document.getElementById("message");
+	content = document.getElementById("output");
 	
-	alert("Hello " + name.value + " your message is: " + message.value);
+	// alert("Hello " + name.value + " your message is: " + message.value);
+	
+	//content.innerText = "Hello " + name.value + " your message is: " + message.value;
+	content.innerHTML = "<div>Hello my name is " + name.value;
+	content.innerHTML +=  " and you message is" + message.value;
+	content.innerHTML +=  " and more stuff";
+	content.innerHTML +=  " yes.. even more</div>";
+	content.innerHTML +=  "<div>and finnally even more stuff!!</div>";
+	
+	
 }
